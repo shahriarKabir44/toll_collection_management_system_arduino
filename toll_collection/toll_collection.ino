8e30d69f91929a91e72b525972c86ba705008ffc
Md. Shahriar Kabir <shahriar1904@cseku.ac.bd>
2022-05-13 13:35:23 +0600

    added RFID

--- toll_collection/toll_collection.ino
+++ toll_collection/toll_collection.ino
@@ -1,7 +1,13 @@
 #include <SoftwareSerial.h>
+#include <MFRC522.h>
 SoftwareSerial myGsm(7, 8);
 #include<Servo.h>
 Servo myservo; 
+
+#define SS_PIN 10
+#define RST_PIN 0
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+
 
 void makeHTTPRequest(String vehicleId,String bridgeId){
   myservo.write(0);
@@ -52,12 +58,60 @@ void setup()
   myGsm.begin(9600);
   Serial.begin(9600);
   delay(500);
-  makeHTTPRequest("1","2");
+  SPI.begin();      // Initiate  SPI bus
+  mfrc522.PCD_Init();   // Initiate MFRC522
+  Serial.println("Approximate your card to the reader...");
+  Serial.println();
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
 }
 
-void loop()
+void loop() 
 {
-}
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  {
+    return;
+  }
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  {
+    return;
+  }
+  //Show UID on serial monitor
+  Serial.print("UID tag :");
+  String content= "";
+  byte letter;
+  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  {
+     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+     Serial.print(mfrc522.uid.uidByte[i], HEX);
+     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  Serial.println();
+  Serial.print("Message : ");
+  content.toUpperCase();
+  if (content.substring(1) == "07 8A 78 7A") //change here the UID of the card/cards that you want to give access
+  {
+    servo1.write(90);
+    Serial.println("Authorized access");
+    Serial.println();
+    digitalWrite(3,HIGH);
+    delay(3000);
+    digitalWrite(3,LOW);
+    servo1.write(0);
+  }
+  
+ else   {
+   digitalWrite(4,HIGH);
+   servo1.write(0);
+    Serial.println(" Access denied");
+    delay(3000);
+     digitalWrite(4,LOW);
+  }
+ 
+} 
 void printResult()
 {
   int chars = 0;
